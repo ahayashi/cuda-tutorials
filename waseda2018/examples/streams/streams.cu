10980fda6f25c7c0e263dd1eb7d995cf3acc2257
Akihiro Hayashi <ahayashi@rice.edu>
2018-01-08 01:34:58 -0500

    Add an example of CUDA Stream

---
+++ waseda2018/examples/streams/streams.cu
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <cuda.h>
+
+long long getCurrentTime() {
+    struct timeval te;
+    gettimeofday(&te, NULL); // get current time
+    long long microseconds = te.tv_sec*1000000LL + te.tv_usec; 
+    return microseconds;
+}
+
+#define CUDA_ERROR_CHECK
+#define CudaSafeCall( err ) __cudaSafeCall( err, __FILE__, __LINE__ )
+
+inline void __cudaSafeCall( cudaError err, const char *file, const int line )
+{
+    #ifdef CUDA_ERROR_CHECK
+    if ( cudaSuccess != err )
+    {
+	fprintf( stderr, "cudaSafeCall() failed at %s:%i : %s\n",
+		 file, line, cudaGetErrorString( err ) );
+	exit( -1 );
+    }
+    #endif
+
+    return;
+}
+
+__global__ void assign(int *A, int streamId)
+{
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x;
+	 i < 1024 * 1024;
+	 i += blockDim.x * gridDim.x) {
+	A[i] += 1024*1024*streamId + i;
+    }
+}
+
+int
+main()
+{
+    cudaStream_t streams[4];
+    int *A[4], *dA[4];
+    int N = 1024*1024;
+
+    for (int i = 0; i < 4; i++) {
+	CudaSafeCall(cudaStreamCreate(&streams[i]));
+	CudaSafeCall(cudaMallocHost((void**)&A[i], sizeof(int) * N));
+	CudaSafeCall(cudaMalloc(&dA[i], N * sizeof(int)));
+    }
+    for (int i = 0; i < 4; i++) {
+	CudaSafeCall(cudaMemcpyAsync(dA[i], A[i], N * sizeof(int),
+				     cudaMemcpyHostToDevice, streams[i]));
+    }
+    for (int i = 0; i < 4; i++) {
+	assign<<<1, 1024, 0, streams[i]>>>(dA[i], i);
+    }
+    for (int i = 0; i < 4; i++) {
+	CudaSafeCall(cudaMemcpyAsync(A[i], dA[i], N * sizeof(int),
+				     cudaMemcpyDeviceToHost, streams[i]));
+    }
+
+    for (int i = 0; i < 4; i++) {
+	CudaSafeCall(cudaStreamSynchronize(streams[i]));
+    }
+    int error = 0;
+    for (int i = 0; i < 4; i++) {
+	for (int j = 0; j < N; j++) {
+	    if (A[i][j] != i * N + j) {
+		error++;
+	    }
+	}
+    }
+    if (!error) {
+	printf("VERIFILED\n");
+    } else {
+	printf("NOT VERIFIED");
+    }
+    
+    return 0;
+}    
